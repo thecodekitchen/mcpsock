8758cd0c92c858ced21b4a3eedf001ff32b9e5e5
Andrew Miller <andy.miller.tostring@hotmail.com>
2025-04-25 10:36:51 -0500

    exported decorator router instead of simple one

--- src/mcpsock/__init__.py
+++ src/mcpsock/__init__.py
@@ -1,5 +1,5 @@
 # Export the main classes for easy import
 from .client import StandaloneClient as WebSocketClient
-from .server import FastMCPWebSocketRouter as WebSocketServer
+from .server import DecoratorRouter as WebSocketServer
 
 __all__ = ['WebSocketClient', 'WebSocketServer']
